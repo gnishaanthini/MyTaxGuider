d3035df09eb4beb0aaec773b13290a0799f3c2db
Nimsara Fernando <nimsara66@gmail.com>
2022-05-01 22:53:46 +0530

    Fix database error in registration hashed password is larger than varchar(20)

--- db/DDL.sql
+++ db/DDL.sql
@@ -1,6 +1,6 @@
 CREATE TABLE Users(
     username VARCHAR(20) NOT NULL,
-    password VARCHAR(20) NOT NULL,
+    password VARCHAR(100) NOT NULL,
     userType ENUM ('Customer', 'Employee', 'Admin') NOT NULL DEFAULT 'Customer',
     PRIMARY KEY (username, password)
 );
