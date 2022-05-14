fc9b6aed4bd838085bdae4c4d551732f1416715a
Nimsara Fernando <nimsara66@gmail.com>
2022-05-14 18:38:18 +0530

    Implement landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,15 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Error, Landing } from './pages/'
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
